6ce72fae3c350f8f6c672391f594a7ff751450d5
Qii <57295427+syauqqii@users.noreply.github.com>
2023-05-26 21:14:20 +0700

    Add file via GitHub Website

---
+++ Utils/ClearConsole.go
@@ -0,0 +1,43 @@
+/* Set package program
+ * Untuk memberitaukan bahwa ini program dari package Utils. */
+package Utils
+
+/* Import Package / Library
+ * Untuk memberikan fitur / fungsi tambahan yang lebih fleksible untuk program ini. */
+import (
+	/* Library os saya gunakan untuk redirect output (os.Stdout) dari command cmd ke console supaya
+	 * dapat membersihkan console serta dieksekusi seperti output new line (bersihkan console). */
+	"os"
+	
+	/* Runtime bawaan go lang yang dapat membuat sebuah instance 'Cmd' dan 'Run'
+	 * Library ini saya gunakan untuk membuat object dari function Command() serta Run(). */
+	"os/exec"
+	
+	/* Library runtime memiliki banyak function, salah satunya saya gunakan untuk mendeteksi os yang digunakan
+	 * dengan cara runtime.GOOS, maka akan mengembalikan string nama OS nya. */
+	"runtime"
+)
+
+/* Function ClearScreen
+ * Fungsi ini saya buat untuk membersihkan console baik di CMD / Terminal.
+ */
+func ClearScreen(){
+	/* Logic Program
+	 * mendaptkan tipe OS dengan menjalankan perintah: runtime.GOOS
+	 * 	-> jika return "windows" :
+	 *		- run command di cmd: cmd (run cmd) -> /c (argument run cmd [cls]) -> cls (bersihkan console)
+	 * 	-> jika return "linux" / "darwin" :
+	 *		- run command di terminal: clear (membersihkan dengan cara seperti di newline sampai bersih, beda dengan windows)
+	 */
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		_ = cmd.Run()
+	} else if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		_ = cmd.Run()
+  	} else {
+		return
+	}
+}
